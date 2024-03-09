efb0a608432ae30b213a47d0f20fe3e7c063c9da
Jonghyun Moon <0803whdgus@gmail.com>
2024-03-09 15:44:19 +0900

    airflow 전역변수 사용

---
+++ dags/dags_bash_with_variable.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+from airflow.models import Variable
+
+with DAG(
+    dag_id="dags_bash_with_variable",
+    schedule="10 9 * * *",
+    start_date=pendulum.datetime(2024, 4, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    # 전역변수 사용 1안: 권고하지 않음(스케줄러의 주기적 dag 파싱 시 variable.get 개수만큼 DB연결해 부하가 발생하기 때문)
+    var_value = Variable.get("sample_key")
+
+    bash_var_1 = BashOperator(
+    task_id="bash_var_1",
+    bash_command=f"echo variable:{var_value}"
+    )
+
+    # 전역변수 사용 2안: jinja 템플릿 변수 사용
+    bash_var_2 = BashOperator(
+    task_id="bash_var_2",
+    bash_command="echo variable:{{var.value.sample_key}}"
+    )
