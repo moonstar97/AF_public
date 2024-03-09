9d184ffdab7f13b0fe94963d98b16f5a417f78db
Jonghyun Moon <0803whdgus@gmail.com>
2024-03-09 16:44:25 +0900

    task 분기처리 with decorator

--- dags/dags_python_with_branch_decorator.py
+++ dags/dags_python_with_branch_decorator.py
@@ -1,11 +1,11 @@
 from airflow import DAG
-from datetime import datetime
+import pendulum
 from airflow.operators.python import PythonOperator
 from airflow.decorators import task
 
 with DAG(
     dag_id='dags_python_with_branch_decorator',
-    start_date=datetime(2024, 3, 1, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2024, 3, 1, tz='Asia/Seoul'),
     schedule=None,
     catchup=False
 ) as dag:
