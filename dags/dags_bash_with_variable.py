fb3df10666049a186da215ba1865cd2f2c564b0a
Jonghyun Moon <0803whdgus@gmail.com>
2024-03-09 16:02:34 +0900

    airflow 전역변수 사용

--- dags/dags_bash_with_variable.py
+++ dags/dags_bash_with_variable.py
@@ -6,7 +6,7 @@ from airflow.models import Variable
 with DAG(
     dag_id="dags_bash_with_variable",
     schedule="10 9 * * *",
-    start_date=pendulum.datetime(2024, 4, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     # 전역변수 사용 1안: 권고하지 않음(스케줄러의 주기적 dag 파싱 시 variable.get 개수만큼 DB연결해 부하가 발생하기 때문)
