06b421dca9659b4daf3a65f0d69d2b73d9ca1ce0
Jonghyun Moon <0803whdgus@gmail.com>
2024-03-30 16:09:04 +0900

    서울시 공공데이터 적재 트러블슈팅

--- dags/dags_seoul_api_corona.py
+++ dags/dags_seoul_api_corona.py
@@ -13,7 +13,8 @@ with DAG(
         task_id='tb_corona19_count_status',
         dataset_nm='TbCorona19CountStatus',
         path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
-        file_name='TbCorona19CountStatus.csv'
+        file_name='TbCorona19CountStatus.csv',
+        base_dt='20240101'
     )
     
     '''서울시 코로나19 백신 예방접종 현황'''
@@ -21,7 +22,8 @@ with DAG(
         task_id='tv_corona19_vaccine_stat_new',
         dataset_nm='tvCorona19VaccinestatNew',
         path='/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
-        file_name='tvCorona19VaccinestatNew.csv'
+        file_name='tvCorona19VaccinestatNew.csv',
+        base_dt='20240101'
     )
 
     tb_corona19_count_status >> tv_corona19_vaccine_stat_new
