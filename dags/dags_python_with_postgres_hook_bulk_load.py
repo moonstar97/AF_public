d1b37aa0b9e963035e611aa9a0fe4c188bece816
Jonghyun Moon <0803whdgus@gmail.com>
2024-03-30 11:43:05 +0900

    custom postgres data insert with hook and bulk load

--- dags/dags_python_with_postgres_hook_bulk_load.py
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -5,7 +5,7 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 with DAG(
         dag_id='dags_python_with_postgres_hook_bulk_load',
-        start_date=pendulum.datetime(2024, 3, 1, tz='Asia/Seoul'),
+        start_date=pendulum.datetime(2023, 3, 16, tz='Asia/Seoul'),
         schedule='0 7 * * *',
         catchup=False
 ) as dag:
