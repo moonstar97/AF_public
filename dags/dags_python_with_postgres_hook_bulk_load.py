b95853e9cc99b9e46852ea18d6199a4afd352b0e
Jonghyun Moon <0803whdgus@gmail.com>
2024-03-30 11:46:33 +0900

    custom postgres data insert with hook and bulk load

--- dags/dags_python_with_postgres_hook_bulk_load.py
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -18,5 +18,7 @@ with DAG(
         python_callable=insrt_postgres,
         op_kwargs={'postgres_conn_id': 'conn-db-postgres-custom',
                    'tbl_nm':'TbCorona19CountStatus_bulk1',
-                   'file_nm':'/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/TbCorona19CountStatus.csv'}
+                   # 코로나 api 불러오는 dag을 unpause 해놔서 파일이 하나만 있음
+                   # {{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}
+                   'file_nm':'/opt/airflow/files/TbCorona19CountStatus/20240316/TbCorona19CountStatus.csv'}
     )
