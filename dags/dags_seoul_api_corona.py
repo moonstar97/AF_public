27c074ba8b3ab9221722fe7540af8bac3029195e
Jonghyun Moon <0803whdgus@gmail.com>
2024-03-30 16:32:18 +0900

    서울시 공공데이터 적재 트러블슈팅

--- dags/dags_seoul_api_corona.py
+++ dags/dags_seoul_api_corona.py
@@ -6,7 +6,6 @@ with DAG(
     dag_id='dags_seoul_api_corona',
     schedule='0 7 * * *',
     start_date=pendulum.datetime(2023, 3, 1, tz='Asia/Seoul'),
-    end_date=pendulum.datetime(2023, 3, 1, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     '''서울시 코로나19 확진자 발생동향'''
